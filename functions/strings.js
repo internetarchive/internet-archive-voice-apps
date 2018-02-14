6434d028111f79500c5ac9bfc78b53c3989564d1
Julia <juliascript@users.noreply.github.com>
2018-02-14 04:15:38 -0500

    [WIP] #6 - Submodules - Move some strings out to individual files  (#7)

---
+++ functions/strings.js
@@ -0,0 +1,32 @@
+module.exports = {
+	fallback: {
+		whatWasThat: "Sorry, what was that?",
+		didntCatchThat: "I didn't catch that",
+		misunderstand: "I'm having trouble understanding you",
+		finalReprompt: "I'm sorry I'm having trouble here. Maybe we should try this again later."
+	},
+	errors: {
+		device: {
+			mediaResponse: "Sorry, your device doesn't support media response."
+		},
+		noInput: {
+			first: "Sorry, I couldn't hear you.",
+			reprompt: "Sorry, can you repeat that?"
+		},
+		unknownInput: {
+			first: "I'm not sure what you said. Can you repeat that?",
+			reprompt: "I still didn't get that."
+		}
+	},
+	suggestion: {
+		artist: {
+			gratefulDead: "Grateful Dead",
+			cowboyJunkies: "Cowboy Junkies",
+			dittyBops: "Ditty Bops",
+			discoBiscuits: "Disco Biscuits",
+			hotButteredRum: "Hot Buttered Rum", 
+			kellerWilliams: "Keller Williams"
+		}
+	},
+	appExit: "Okay, let's try this again later."
+}
