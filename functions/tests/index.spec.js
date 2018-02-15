e20581f033bf71f900d5b27e079ac149def889eb
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-02-15 20:54:46 +0100

    add mocking for intend handling

--- functions/tests/index.spec.js
+++ functions/tests/index.spec.js
@@ -1,8 +1,14 @@
 const {expect} = require('chai');
+const mocking = require('./_utils/mocking');
+const {buildIntentRequest, MockResponse} = require('./_utils/mocking');
 const {playMedia} = require('..');
 
 describe('playMedia', () => {
   it('should be defined', () => {
     expect(playMedia).to.be.ok;
+  });
+
+  it('should handle common intend', () => {
+    playMedia(buildIntentRequest({action: 'input.welcome'}), new MockResponse());
   });
 });
