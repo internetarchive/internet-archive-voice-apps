0007cab03b8dbb0123ebddf9aa1c03cbeb171406
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-02-15 23:06:06 +0100

    returned user has different response than new one

--- functions/tests/index.spec.js
+++ functions/tests/index.spec.js
@@ -7,9 +7,23 @@ describe('playMedia', () => {
     expect(index.playMedia).to.be.ok;
   });
 
-  it('should handle welcome action', () => {
-    const res = new MockResponse();
-    index.playMedia(buildIntentRequest({action: 'input.welcome'}), res);
-    expect(res.speech()).to.contain('Welcome back, choose an artist.');
+  describe('welcome action', () => {
+    it('should handle for a new user', () => {
+      const res = new MockResponse();
+      index.playMedia(buildIntentRequest({
+        action: 'input.welcome',
+        lastSeen: null,
+      }), res);
+      expect(res.speech()).to.not.contain('Welcome back');
+      expect(res.speech()).to.contain('Welcome to the live music collection at the Internet Archive.');
+    });
+
+    it('should handle for return user', () => {
+      const res = new MockResponse();
+      index.playMedia(buildIntentRequest({
+        action: 'input.welcome',
+      }), res);
+      expect(res.speech()).to.contain('Welcome back, choose an artist.');
+    });
   });
 });
