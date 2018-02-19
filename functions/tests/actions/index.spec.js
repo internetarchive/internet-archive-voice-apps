a4703a4a1220801bdb72f78403abaec7870cecad
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-02-19 22:56:39 +0100

    check actions to have a key

--- functions/tests/actions/index.spec.js
+++ functions/tests/actions/index.spec.js
@@ -5,7 +5,7 @@ describe('actions', () => {
   describe('defaultActions', () => {
     it('should grab all actions and map to file name', () => {
       const actions = defaultActions();
-      expect(actions).to.have.property('welcome');
+      expect(actions).to.have.key('welcome');
     });
   });
 });
