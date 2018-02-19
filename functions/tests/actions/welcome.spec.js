b666b06dd55d6b5bf0e559ab9212688cc99088c1
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-02-19 23:39:16 +0100

    add repeat action

--- functions/tests/actions/welcome.spec.js
+++ functions/tests/actions/welcome.spec.js
@@ -2,19 +2,18 @@ const {expect} = require('chai');
 const rewire = require('rewire');
 const sinon = require('sinon');
 const mockApp = require('../_utils/mocking/app');
+const mockDialog = require('../_utils/mocking/dialog');
 const welcome = rewire('../../actions/welcome');
 
 describe('actions', () => {
   let dialog;
   beforeEach(() => {
-    dialog = {
-      ask: sinon.spy(),
-    };
+    dialog = mockDialog();
     welcome.__set__('dialog', dialog);
   });
 
   describe('welcome', () => {
-    it('should ...', () => {
+    it('should ask user', () => {
       let app = mockApp();
       welcome.handler(app);
       expect(dialog.ask).have.been.calledOnce;
