b389c203361d1d53789e730e67c08ce602d9410d
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-02-20 16:51:43 +0100

    add commont repair handler for no-input and unknown actions

--- functions/actions/helpers/handlers.js
+++ functions/actions/helpers/handlers.js
@@ -1,25 +1,12 @@
 const path = require('path');
 
-/**
- * Strip file name from full path to get the name of action
- *
- * @param filename
- * @returns {string}
- */
-const actionNameByFileName = (filename) =>
-  path.basename(filename, path.extname(filename));
-
 module.exports = {
   /**
-   * check is it handler of the action
+   * Strip file name from full path to get the name of action
    *
-   * @param filename {string} path to the handler
-   * @param action {string}
-   * @returns {boolean}
+   * @param filename {string}
+   * @returns {string}
    */
-  isItHandlerOfAction: (filename, action) => {
-    return actionNameByFileName(filename) === action;
-  },
-
-  actionNameByFileName,
+  actionNameByFileName: filename =>
+    path.basename(filename, path.extname(filename)),
 };
--- functions/actions/unknown.js
+++ functions/actions/unknown.js
@@ -1,41 +1,15 @@
-const dialog = require('../dialog');
-const {getLastReprompt, getLastSuggestions} = require('../state/dialog');
-const {getLastAction, getLastRepetitionCount} = require('../state/repetition');
 const intentStrings = require('../strings').intents.unknown;
-
-
-/**
- * handle no-input intent
- *
- * @param app
- */
-function handler (app) {
-  let count = 0;
-
-  //TODO: should works implicitly (without 'unknown')
-  if (getLastAction(app) === 'unknown') {
-    count = getLastRepetitionCount(app);
-  }
-
-  const suggestions = getLastSuggestions(app);
-
-  switch (count) {
-    case 0:
-      dialog.ask(app, intentStrings.first, suggestions);
-      break;
-    case 1:
-      dialog.ask(
-        app,
-        intentStrings.reprompt.replace('${reprompt}', getLastReprompt(app)),
-        suggestions
-      );
-      break;
-    default:
-      dialog.tell(app, intentStrings.fallback);
-      break;
-  }
-}
+const {buildRapairHandler} = require('./helpers/repair-handler-builder');
+const {actionNameByFileName} = require('./helpers/handlers');
 
 module.exports = {
-  handler,
+  /**
+   * handle no-input intent
+   *
+   * @param app
+   */
+  handler: buildRapairHandler(
+    actionNameByFileName(__filename),
+    intentStrings
+  ),
 };
