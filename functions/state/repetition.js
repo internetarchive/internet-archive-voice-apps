4189fe238c76da3901e8e15ee1d04cc0871c0f31
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-02-20 01:50:44 +0100

    should set to zero number of repetitions if we get new action

--- functions/state/repetition.js
+++ functions/state/repetition.js
@@ -8,8 +8,13 @@ const {getData, setData} = require('./helpers').group('repetition');
  * @param action {string} name of action
  */
 function storeAction (app, action) {
+  if (getLastAction(app) === action) {
+    return;
+  }
+
   setData(app, Object.assign({}, getData(app), {
     action,
+    count: 0,
   }));
 }
 
@@ -23,7 +28,28 @@ function getLastAction (app) {
   return _.at(getData(app), 'action')[0];
 }
 
+/**
+ * store action count
+ *
+ * @param count
+ */
+function storeRepetitionCount(app, count) {
+  setData(app, Object.assign({}, getData(app), {
+    count,
+  }));
+}
+
+/**
+ * get last action count
+ * @returns {*}
+ */
+function getLastRepetitionCount(app) {
+  return _.at(getData(app), 'count')[0];
+}
+
 module.exports = {
   getLastAction,
   storeAction,
+  getLastRepetitionCount,
+  storeRepetitionCount,
 };
