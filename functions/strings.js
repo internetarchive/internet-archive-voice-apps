b59fbb072aa8d769f61941eef61dc9f33dee0a0e
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-02-20 16:03:43 +0100

    add unknown intent handler

--- functions/strings.js
+++ functions/strings.js
@@ -35,6 +35,11 @@ module.exports = {
       reprompt: 'Sorry, can you repeat that? ${reprompt}',
       fallback: "I'm sorry I'm having trouble here. Maybe we should try this again later.",
     },
+    unknown: {
+      first: "I'm not sure what you said. Can you repeat that?",
+      reprompt: "I still didn't get that. ${reprompt}",
+      fallback: "I'm sorry I'm having trouble here. Maybe we should try this again later.",
+    },
     welcome: {
       suggestions: [
         'Grateful Dead',
