c733f8c95405ab2f7af2e594a5c24e2db95daa69
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-02-22 17:53:52 +0100

    use strings templates for customize song messages

--- functions/tests/dialog/audio.spec.js
+++ functions/tests/dialog/audio.spec.js
@@ -1,32 +1,57 @@
 const {expect} = require('chai');
-const {audio} = require('../../dialog/audio');
+const {song} = require('../../dialog/audio');
 const mockApp = require('../_utils/mocking/app');
+const mustache = require('mustache');
+const strings = require('../../strings').dialog.song;
 
 describe('dialog', () => {
-  describe('audio', () => {
-    let app;
+  let app;
 
-    beforeEach(() => {
-      app = mockApp();
-    });
+  beforeEach(() => {
+    app = mockApp();
+  });
 
+  describe('song', () => {
     it('should share audio response with user', () => {
       const audioURL = 'https://archive.org/download/gd73-06-10d1t01.mp3';
-      const coverage = 'https://archive.org/gd73-06-10.sbd.hollister.174.sbeok.shnf/RFKJune73extras/Covers/GD6-10-73backtyedie.jpg';
+      const coverage = 'Washington, DC';
+      const imageURL = 'https://archive.org/gd73-06-10.sbd.hollister.174.sbeok.shnf/RFKJune73extras/Covers/GD6-10-73backtyedie.jpg';
       const suggestions = ['rewind', 'next'];
       const title = 'Morning Dew';
       const track = 1;
       const year = 1973;
 
-      audio(app, {audioURL, coverage, suggestions, title, track, year});
+      song(app, {
+        audioURL,
+        coverage,
+        imageURL,
+        suggestions,
+        title,
+        track,
+        year,
+      });
 
       expect(app.ask).to.be.calledOnce;
       expect(app.buildMediaResponse).to.be.calledOnce;
       expect(app.buildMediaObject).to.be.calledOnce;
       expect(app.buildRichResponse).to.be.calledOnce;
       expect(app.addMediaObjects).to.be.calledOnce;
-      expect(app.setDescription).to.be.calledOnce;
-      expect(app.setImage).to.be.calledOnce;
+      expect(app.setDescription).to.be.calledWith(
+        mustache.render(mustache.render(
+          strings.description,
+          {audioURL, coverage, imageURL, suggestions, title, track, year}
+        ))
+      );
+      expect(app.setImage).to.be.calledWith(
+        imageURL,
+        app.Media.ImageType.LARGE
+      );
+      expect(app.addSimpleResponse).to.be.calledWith(
+        mustache.render(mustache.render(
+          strings.description,
+          {audioURL, coverage, imageURL, suggestions, title, track, year}
+        ))
+      );
       expect(app.addSuggestionLink).to.be.calledOnce;
       expect(app.addSuggestions).to.be.calledWith(suggestions);
     });
