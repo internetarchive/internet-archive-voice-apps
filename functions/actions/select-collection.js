2cfbb0f10d62466669b720ae99a988143ac2155c
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-02-23 23:11:57 +0100

    store selected collection

--- functions/actions/select-collection.js
+++ functions/actions/select-collection.js
@@ -2,12 +2,18 @@ const debug = require('debug')('ai:actions:select-collection:debug');
 
 const dialog = require('../dialog');
 const collection = require('../provider/collection');
+const querySlots = require('../state/query');
 
 function handler (app) {
   debug(`Start handle select collection`);
+
   const collectionId = app.getArgument('collection');
+  querySlots.setSlot(app, 'collection', collectionId);
+
   return collection.fetchDetails(collectionId)
     .then(details => {
+      // TODO: we could add storage of fetch collection
+      // if we will need title of collection later
       dialog.ask(app, {
         speech: `Ok you selected ${details.title}.
                  What artist would you like to listen to, e.g.
