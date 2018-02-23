e5a08bd56e9cc3b9bc02d2099eb076b1f166fb0e
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-02-23 16:27:52 +0100

    request details about collection

--- functions/config.js
+++ functions/config.js
@@ -1,6 +1,5 @@
 module.exports = {
   endpoints: {
-    ALBUM_URL: 'https://web.archive.org/metadata/{{id}}',
     ALBUMS_OF_CREATOR_URL: 'https://web.archive.org/advancedsearch.php' +
     '?q=collection:({{creatorId}})' +
     '&fl[]={{fields}}' +
@@ -8,6 +7,7 @@ module.exports = {
     '&rows={{limit}}' +
     '&page={{page}}' +
     '&output=json',
+    COLLECTION_URL: 'https://web.archive.org/metadata/{{id}}',
     SONG_URL: 'https://archive.org/download/{{albumId}}/{{filename}}',
   },
 
