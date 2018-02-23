285ed96c30284d332bc59a2a4df8b3eab0d9d2d7
Bimlendra <bimlendra.singh@gmail.com>
2018-02-23 19:04:18 +0530

    update welcome message

--- functions/actions/welcome.js
+++ functions/actions/welcome.js
@@ -11,18 +11,16 @@ function handler (app) {
   // askAudio(app, "Test Song", "https://ia802307.us.archive.org/20/items/gd73-06-10.sbd.hollister.174.sbeok.shnf/RFKJune73extras/Booklet/center_vbr.mp3", suggestions);
 
   // let cardTitle = 'Welcome';
-  let reprompt = "<speak>Waiting for your response. <break time='.2s'/> What artist would you like to listen to?</speak>";
+  let repromptText = '<speak>Waiting for your response. <break time=".2s"/> What artist would you like to listen to?</speak>';
   // let cardOutput = 'Welcome to the live music collection at the Internet Archive. What artist would you like to listen to? For example The Ditty Bops, The Grateful Dead or The Cowboy Junkies.';
-  let speech = "<speak><audio src='https://s3.amazonaws.com/gratefulerrorlogs/CrowdNoise.mp3' />  Welcome to the live music collection at the Internet Archive.<break time='.5s'/> What artist would you like to listen to? <break time='.5s'/>  For example, the ditty bops, the grateful dead, or the cowboy junkies.  </speak>";
+  let speechOutput = '<audio src="https://s3.amazonaws.com/gratefulerrorlogs/CrowdNoise.mp3" />  Welcome to music at the Internet Archive. Would you like to listen to music from our collections of 78s or Live Concerts?';
   // let speechOutput = "<speak>Welcome to the live music collection at the Internet Archive.<break time='.5s'/> What artist would you like to listen to? <break time='.5s'/>  For example, the ditty bops, the grateful dead, or the cowboy junkies. </speak>";
 
   if (app.getLastSeen() !== null) {
-    speech = "<speak>Welcome back, choose an artist.<break time='.5s'/> For example, the ditty bops, the grateful dead, or the cowboy junkies. </speak>";
+    speechOutput = 'Welcome back, Would you like to listen to music from our collections of 78s or Live Concerts?';
   }
 
-  const suggestions = intentStrings.suggestions;
-
-  dialog.ask(app, {speech, reprompt, suggestions});
+  dialog.ask(app, speechOutput, repromptText, intentStrings.suggestions);
 }
 
 module.exports = {
