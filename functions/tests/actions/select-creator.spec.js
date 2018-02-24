12abd83be83fc31b5604644347b59b48464c6924
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-02-24 20:13:22 +0100

    use general fetch of artist albums

--- functions/tests/actions/select-creator.spec.js
+++ functions/tests/actions/select-creator.spec.js
@@ -7,10 +7,12 @@ const querySlots = require('../../state/query');
 const mockApp = require('../_utils/mocking/app');
 const mockDialog = require('../_utils/mocking/dialog');
 const mockSearchCollection = require('../_utils/mocking/provider/collection');
+const mockSearchCreator = require('../_utils/mocking/provider/creator');
 
 describe('actions', () => {
   let app;
   let collection;
+  let creator;
   let dialog;
 
   beforeEach(() => {
@@ -18,7 +20,15 @@ describe('actions', () => {
     collection = mockSearchCollection({
       fetchDetailsResponse: {title: 'Cool Band'}
     });
+    creator = mockSearchCreator({
+      fetchAlbumsResponse: {
+        items: [
+          {title: 'Excellent Concert', year: 1999},
+        ],
+      },
+    });
     action.__set__('collection', collection);
+    action.__set__('creator', creator);
     action.__set__('dialog', dialog);
     app = mockApp({
       argument: 'cool-band',
@@ -30,6 +40,7 @@ describe('actions', () => {
       return action.handler(app)
         .then(() => {
           expect(collection.fetchDetails).to.be.calledWith('cool-band');
+          expect(creator.fetchAlbums).to.be.calledWith('cool-band');
           expect(dialog.ask).to.be.calledOnce;
         });
     });
