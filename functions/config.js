6f205dff9b413b55a370dd34af44bd7dd1e37776
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-02-25 11:29:34 +0100

    quick and dirty implementation of select-coverage-and-date

--- functions/config.js
+++ functions/config.js
@@ -15,6 +15,13 @@ module.exports = {
     '&rows={{limit}}' +
     '&page={{page}}' +
     '&output=json',
+    QUERY_COLLECTIONS_URL: 'https://web.archive.org/advancedsearch.php' +
+    '?q={{condition}}' +
+    '&fl[]={{fields}}' +
+    '&sort[]={{sort}}' +
+    '&rows={{limit}}' +
+    '&page={{page}}' +
+    '&output=json',
     SONG_URL: 'https://archive.org/download/{{albumId}}/{{filename}}',
   },
 
