b5e85d41dc20eebcf532c53eca84ec61b52e6e06
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-02-25 13:21:43 +0100

    play 1st albumn when we get place and year

--- functions/actions/select-coverage-and-date.js
+++ functions/actions/select-coverage-and-date.js
@@ -2,30 +2,39 @@ const debug = require('debug')('ia:actions:select-creator:debug');
 
 const dialog = require('../dialog');
 
+const search = require('../provider/audio');
 const creatorProvider = require('../provider/creator');
+const playlist = require('../state/playlist');
 const querySlots = require('../state/query');
 
 function handler (app) {
   debug('Handle select coverage and date');
+
   const coverage = app.getArgument('coverages');
   if (coverage) {
     debug(`get coverages ${coverage}`);
     querySlots.setSlot(app, 'converage', coverage);
   }
 
-  const year = app.getArgument('date');
-  if (year) {
-    debug(`get year ${year}`);
-    querySlots.setSlot(app, 'date', year);
+  const date = app.getArgument('date');
+  debug(`get date ${date}`);
+
+  let year = app.getArgument('year');
+  debug(`get year ${year}`);
+
+  if (date && !year) {
+    // extract year
+    year = (new Date(date)).getFullYear();
   }
 
-  // TODO: if we have coverage, date, and creator
-  // we could get album id
+  querySlots.setSlot(app, 'year', year);
 
   // TODO: maybe we can catch album with less information
   // for example when only one artist was played in this town
-  // at that year.
+  // at that year. Thus Town and year could be enough.
 
+  // If we have coverage, date, and creator
+  // we could get album id
   const creatorId = querySlots.getSlot(app, 'creator');
   creatorProvider
     .fetchAlbumsByQuery({
@@ -34,8 +43,9 @@ function handler (app) {
       year,
     })
     .then(albums => {
-      debug('get albums');
+      debug(`get ${albums.items.length} albums`);
       debug(albums.items);
+      let albumId = null;
       switch (albums.items.length) {
         case 0:
           // TODO: there is no such album
@@ -44,9 +54,10 @@ function handler (app) {
           break;
         case 1:
           // TODO: play it!
-          dialog.ask(app, {
-            speech: `DEBUG: play album: ${albums.items[0].title}`,
-          });
+          // dialog.ask(app, {
+          //   speech: `DEBUG: play album: ${albums.items[0].title}`,
+          // });
+          albumId = albums.items[0].identifier;
           break;
         default:
           // TODO: we get more than 1 album
@@ -57,15 +68,45 @@ function handler (app) {
           // - https://archive.org/details/gd73-06-09.sbd.hollister.172.sbeok.shnf
           // - https://archive.org/details/gd73-06-10.sbd.hollister.174.sbeok.shnf
           // we should play they are together.
-          const names = albums.items
-            .slice(0, 10)
-            .map(album => `${album.coverage} ${album.year}`)
-            .join(', ');
+          // const names = albums.items
+          //   .slice(0, 10)
+          //   .map(album => `${album.coverage} ${album.year}`)
+          //   .join(', ');
 
-          dialog.ask(app, {
-            speech: `DEBUG: we get ${albums.items.length} albums: ${names}`,
-          });
+          // dialog.ask(app, {
+          //   speech: `DEBUG: we get ${albums.items.length} albums: ${names}`,
+          // });
+
+          // TODO: for the moment just play 1st album
+          // but we should make playlist from all albums
+          albumId = albums.items[0].identifier;
           break;
+      }
+
+      if (albumId) {
+        search.fetchAlbumDetails(albumId)
+          .then(album => {
+            debug(`We get album ${JSON.stringify(album)}`);
+            const songs = album.songs
+              .map((song, idx) => Object.assign({}, song, {
+                audioURL: search.getSongUrlByAlbumIdAndFileName(albumId, song.filename),
+                coverage: album.coverage,
+                imageURL: `https://archive.org/services/img/${albumId}`,
+                // TODO : add recommendations
+                suggestions: ['TODO'],
+                track: idx + 1,
+                year: album.year,
+              }));
+
+            playlist.create(app, songs);
+
+            dialog.playSong(app, playlist.getCurrentSong(app));
+          })
+          .catch(err => {
+            dialog.ask(app, {
+              speech: `We got an error: ${JSON.stringify(err)}. Do you want to try again?`
+            });
+          });
       }
     });
 }
