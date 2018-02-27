e3b100aa5c4dd2824caf8e35f1759790ef2ec789
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-02-27 15:53:06 +0100

    humanize suggestions before substitute to prompt

--- functions/tests/humanize/list.spec.js
+++ functions/tests/humanize/list.spec.js
@@ -4,10 +4,16 @@ const {list} = require('../../humanize');
 describe('humanize', () => {
   describe('list', () => {
     describe('toSoftString', () => {
-      it('should converts a list of items to a human readable string', () => {
+      it('should converts a array of items to a human readable string', () => {
         expect(
           list.toFriendlyString(['A', 'B', 'C'], {ends: ' or '})
         ).to.be.equal('A, B or C');
+      });
+
+      it('should return empty string for empty array', () => {
+        expect(
+          list.toFriendlyString()
+        ).to.be.equal('');
       });
     });
   });
