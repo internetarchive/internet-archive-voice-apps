311d244dd80d7d71cce408122b6c3ed505ad8c03
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-02-28 02:02:48 +0100

    fetch collection details to resolve creator

--- functions/slots/extensions/resolvers/creator.js
+++ functions/slots/extensions/resolvers/creator.js
@@ -1,6 +1,16 @@
+const debug = require('debug')('ia:resolver:creator');
+
+const collection = require('../../../provider/collection');
+
+/**
+ * Handle context to fetch creator details
+ *
+ * @param creatorId
+ * @returns {Promise}
+ */
 function handler ({creatorId} = {}) {
-  // TODO: fetch data to resolve creatorId to something more useful
-  return Promise.resolve();
+  debug('start handling', creatorId);
+  return collection.fetchDetails(creatorId);
 }
 
 module.exports = {
