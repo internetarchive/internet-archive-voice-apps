91e08ebcd0daebeb2dae743da6699ca390f057e3
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-02-28 02:02:48 +0100

    fetch collection details to resolve creator

--- functions/tests/slots/extensions/resolvers/creator.spec.js
+++ functions/tests/slots/extensions/resolvers/creator.spec.js
@@ -1,8 +1,22 @@
 const {expect} = require('chai');
+const rewire = require('rewire');
 
-const creator = require('../../../../slots/extensions/resolvers/creator');
+const creator = rewire('../../../../slots/extensions/resolvers/creator');
+
+const mockSearchCollection = require('../../../_utils/mocking/provider/collection');
 
 describe('slots', () => {
+  let collection;
+
+  beforeEach(() => {
+    collection = mockSearchCollection({
+      fetchDetailsResponse: {
+        title: 'Cool Band',
+      },
+    });
+    creator.__set__('collection', collection);
+  });
+
   describe('extensions', () => {
     describe('resolver', () => {
       describe('creator', () => {
@@ -13,6 +27,15 @@ describe('slots', () => {
         describe('handler', () => {
           it('should return promise', () => {
             expect(creator.handler()).to.have.property('then');
+          });
+
+          it('should fetch metadata from IA and return object with title', () => {
+            creator.handler({
+              creatorId: 'cool-band',
+            }).then((res) => {
+              expect(collection.fetchDetails).to.be.calledWith('cool-band');
+              expect(res).to.have.property('title').to.equal('Cool Band');
+            });
           });
         });
       });
