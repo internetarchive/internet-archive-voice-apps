7b50628f8d9a75a5f6ca4e330639d8100084e987
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-03-01 00:44:11 +0100

    :construction_worker: fix style

--- functions/tests/actions/welcome.spec.js
+++ functions/tests/actions/welcome.spec.js
@@ -1,6 +1,5 @@
 const {expect} = require('chai');
 const rewire = require('rewire');
-const sinon = require('sinon');
 const mockApp = require('../_utils/mocking/app');
 const mockDialog = require('../_utils/mocking/dialog');
 const welcome = rewire('../../actions/welcome');
