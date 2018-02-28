44ec8aa9c160caa9a728a7876728032ad37c9a96
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-03-01 00:14:27 +0100

    provier creator could fetch popular creators

--- functions/tests/provider/creators.spec.js
+++ functions/tests/provider/creators.spec.js
@@ -1,16 +1,45 @@
 const {expect} = require('chai');
-const creators = require('../../provider/creators');
+const fetchMock = require('fetch-mock');
+fetchMock.config.overwriteRoutes = true;
+const rewire = require('rewire');
+
+const creators = rewire('../../provider/creators');
+
+const popularAlbums = require('./fixtures/popular-of-etree.json');
 
 describe('providers', () => {
   describe('creators', () => {
-    describe('fetchCreators', () => {
-      return creators.fetchCreators({
+    beforeEach(() => {
+      creators.__set__(
+        'fetch',
+        fetchMock
+          .sandbox()
+          .get('begin:https://web.archive.org/advancedsearch.php', popularAlbums)
+      );
+    });
 
-      })
-        .then((res) => {
-          expect(res).to.have.property('item').with.deep.members([{
-          }])
-        });
+    describe('fetchCreators', () => {
+      it('should fetch popular creators', () => {
+        return creators
+          .fetchCreators({
+            coverage: 'london',
+            creatorId: 'theband',
+            collectionId: '80s',
+            year: '2020',
+          })
+          .then((res) => {
+            expect(res).to.have.property('items').with.deep.members([{
+              creator: 'Grateful Dead',
+              identifier: 'GratefulDead',
+            }, {
+              creator: 'Phil Lesh and Friends',
+              identifier: 'PhilLeshandFriends',
+            }, {
+              creator: 'Disco Biscuits',
+              identifier: 'DiscoBiscuits',
+            }])
+          });
+      });
     });
   });
 });
