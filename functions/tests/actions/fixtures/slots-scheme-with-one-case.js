073c87b1728b900033ccbd0bab5981562e270f01
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-03-02 12:40:45 +0100

    should set defaults to slots

--- functions/tests/actions/fixtures/slots-scheme-with-one-case.js
+++ functions/tests/actions/fixtures/slots-scheme-with-one-case.js
@@ -1,4 +1,24 @@
 module.exports = {
+  name: 'one dialog',
+
+  /**
+   * default values for slots
+   * (we don't need to define those slots in `slots` field)
+   */
+  defaults: {
+    sort: 'random',
+  },
+
+  /**
+   * slots which we need for fulfillement
+   */
+  slots: [
+    'collection',
+    'creatorId',
+    'coverage',
+    'year',
+  ],
+
   acknowledges: [
     '{{coverage}} - good place!',
     '{{coverage}} {{year}} - great choice!',
@@ -50,16 +70,6 @@ module.exports = {
       'Do you have a specific city and year in mind, like {{suggestions.values.0}}, or would you like me to play something randomly?',
     ],
   }],
-
-  /**
-   * slots which we need for fulfillement
-   */
-  slots: [
-    'collection',
-    'creatorId',
-    'coverage',
-    'year',
-  ],
 
   /**
    * feeder which we should call once we get all slots
