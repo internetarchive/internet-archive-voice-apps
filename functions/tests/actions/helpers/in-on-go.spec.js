2b0cec4c3a7650a273c2e08bfe88065638286efd
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-03-05 21:11:54 +0100

    add unit test

---
+++ functions/tests/actions/helpers/in-on-go.spec.js
@@ -0,0 +1,19 @@
+const {expect} = require('chai');
+
+const builder = require('../../../actions/helpers/in-one-go');
+
+const strings = require('./fixtures/in-on-go.json');
+
+describe('actions', () => {
+  describe('high-order handlers', () => {
+    describe('in one go handler', () => {
+      it('should have build method', () => {
+        expect(builder).to.have.property('build');
+      });
+
+      it('should create object with handler method', () => {
+        expect(builder.build(strings)).to.have.property('handler');
+      });
+    });
+  });
+});
