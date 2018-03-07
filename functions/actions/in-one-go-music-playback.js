ecfd28ad38fa02021f905bc298cdfb86d267bd16
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-03-07 21:52:14 +0100

    generalize in one go music playback

--- functions/actions/in-one-go-music-playback.js
+++ functions/actions/in-one-go-music-playback.js
@@ -1,6 +1,6 @@
 const playlist = require('../state/playlist');
 const query = require('../state/query');
-const strings = require('../strings').intents.inOneGoArtistPlayback;
+const strings = require('../strings').intents.inOneGoMusicPlayback;
 
 const inOneGo = require('./high-order-handlers/in-one-go');
 
