7543624e810e46caed294ae3a70f0d8475c73cbd
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-03-07 13:28:42 +0100

    move has next method to natural order strategy

--- functions/extensions/orders/natural.js
+++ functions/extensions/orders/natural.js
@@ -17,6 +17,24 @@ class NaturalOrderStrategy {
   }
 
   /**
+   * Do we have next item
+   *
+   * @param app
+   * @param slots
+   * @param playlist
+   * @returns {boolean}
+   */
+  hasNext({app, query, playlist}) {
+    const cursor = playlist.getExtra(app).cursor;
+    // Isn't it the last album
+    if (cursor.current.album < cursor.total.albums - 1) {
+      return true;
+    }
+    // Isn't it the last song?
+    return cursor.current.song < cursor.total.songs - 1;
+  }
+
+  /**
    * Chup few songs at the start because we've already fetched them
    *
    * @param songs
