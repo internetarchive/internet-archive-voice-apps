30303db62e04206047413a0c33498eb825e7dcb8
Bimlendra <bimlendra.singh@gmail.com>
2018-03-09 18:25:10 +0530

    resolved the issue of Firebase Config & put dashbot, bespoken values in config

--- functions/tests/index.spec.js
+++ functions/tests/index.spec.js
@@ -1,16 +1,24 @@
 const {expect} = require('chai');
 const rewire = require('rewire');
 const sinon = require('sinon');
+var index, configStub, adminInitStub, functions, admin;
 
 const {buildIntentRequest, MockResponse} = require('./_utils/mocking');
-const index = rewire('..');
 
 const {wait} = require('./_utils/wait');
 
 describe('playMedia', () => {
   let res;
 
-  beforeEach(() => {
+  before(() => {
+    admin = require('firebase-admin');
+    adminInitStub = sinon.stub(admin, 'initializeApp');
+    // Next we stub functions.config(). Normally config values are loaded from Cloud Runtime Config;
+    // here we'll just provide some fake values for firebase.databaseURL and firebase.storageBucket
+    // so that an error is not thrown during admin.initializeApp's parameter check
+    functions = require('firebase-functions');
+    configStub = sinon.stub(functions, 'config').returns(require(`../../.runtimeconfig.json`));
+    index = rewire('..');
     res = new MockResponse();
   });
 
@@ -40,5 +48,10 @@ describe('playMedia', () => {
         expect(warning.getCall(0).args[0]).to.includes(action);
         expect(warning).to.be.calledOnce;
       });
+  });
+  after(() => {
+    // Restoring our stubs to the original methods.
+    configStub.restore();
+    adminInitStub.restore();
   });
 });
--- functions/tests/integration/welcome.spec.js
+++ functions/tests/integration/welcome.spec.js
@@ -3,10 +3,21 @@
  */
 
 const {expect} = require('chai');
-const index = require('../..');
+var index, configStub, adminInitStub, functions, admin;
 const {buildIntentRequest, MockResponse} = require('../_utils/mocking');
+const sinon = require('sinon');
 
 describe('integration', () => {
+  before(() => {
+    admin = require('firebase-admin');
+    adminInitStub = sinon.stub(admin, 'initializeApp');
+    // Next we stub functions.config(). Normally config values are loaded from Cloud Runtime Config;
+    // here we'll just provide some fake values for firebase.databaseURL and firebase.storageBucket
+    // so that an error is not thrown during admin.initializeApp's parameter check
+    functions = require('firebase-functions');
+    configStub = sinon.stub(functions, 'config').returns(require(`../../../.runtimeconfig.json`));
+    index = require('../..');
+  });
   describe('unknown', () => {
     it('should handle for a new user', () => {
       const res = new MockResponse();
@@ -25,5 +36,10 @@ describe('integration', () => {
       }), res);
       expect(res.speech()).to.contain('Welcome to the live music collection at the Internet Archive.');
     });
+  });
+  after(() => {
+    // Restoring our stubs to the original methods.
+    configStub.restore();
+    adminInitStub.restore();
   });
 });
