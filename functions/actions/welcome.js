3c82c401a5ee60e049a8d9707358e7d7f7b17c71
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-03-09 12:35:13 +0100

    change greeting message to 'Welcome to music at the Internet Archive.'

--- functions/actions/welcome.js
+++ functions/actions/welcome.js
@@ -1,6 +1,7 @@
+const _ = require('lodash');
+
 const dialog = require('../dialog');
 const welcomeStrings = require('../strings').intents.welcome;
-const greetingStrings = require('../strings').statements.greeting;
 
 /**
  * handle welcome intent
@@ -14,11 +15,11 @@ function handler (app) {
   // let cardTitle = 'Welcome';
   let reprompt = welcomeStrings.reprompt + ' ' + welcomeStrings.speech;
   // let cardOutput = 'Welcome to the live music collection at the Internet Archive. What artist would you like to listen to? For example The Ditty Bops, The Grateful Dead or The Cowboy Junkies.';
-  let speech = '<speak> <audio src="https://s3.amazonaws.com/gratefulerrorlogs/CrowdNoise.mp3" />' + greetingStrings.welcome.liveMusicCollection + ' ' + welcomeStrings.speech + '</speak>';
+  let speech = '<speak> <audio src="https://s3.amazonaws.com/gratefulerrorlogs/CrowdNoise.mp3" />' + _.sample(welcomeStrings.acknowledges) + ' ' + welcomeStrings.speech + '</speak>';
   // let speechOutput = "<speak>Welcome to the live music collection at the Internet Archive.<break time='.5s'/> What artist would you like to listen to? <break time='.5s'/>  For example, the ditty bops, the grateful dead, or the cowboy junkies. </speak>";
 
   if (app.getLastSeen() !== null) {
-    speech = greetingStrings.welcomeBack + ' ' + welcomeStrings.speech;
+    speech = _.sample(welcomeStrings.acknowledges) + ' ' + welcomeStrings.speech;
   }
 
   dialog.ask(app, Object.assign({}, welcomeStrings, {speech, reprompt}));
