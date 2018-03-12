ad74c7cc7ce5ccef71a60c001a6de1b778ac5912
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-03-12 21:49:12 +0100

    use console.<function>.bind only when we have this logger

--- functions/utils/logger.js
+++ functions/utils/logger.js
@@ -8,11 +8,17 @@ const loggerBuilder = require('debug');
  */
 module.exports = (name) => {
   const debug = loggerBuilder(`${name}:debug`);
-  debug.log = console.debug.bind(console);
+  if (console.debug) {
+    debug.log = console.debug.bind(console);
+  }
   const error = loggerBuilder(`${name}:error`);
-  error.log = console.error.bind(console);
+  if (console.error) {
+    error.log = console.error.bind(console);
+  }
   const warning = loggerBuilder(`${name}:warning`);
-  warning.log = console.warn.bind(console);
+  if (console.warn) {
+    warning.log = console.warn.bind(console);
+  }
 
   return {
     debug,
