bdbf557904d2875206b796829ac1bdd942db2b4c
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-03-13 19:41:41 +0100

    patch logger scope variable inside of wrapper module

--- functions/src/utils/logger.js
+++ functions/src/utils/logger.js
@@ -1,4 +1,35 @@
+const functions = require('firebase-functions');
+const _ = require('lodash');
+patchDebugScopeEnvVariable();
 const loggerBuilder = require('debug');
+
+/**
+ * Patch DEBUG environment variable (process.env.DEBUG)
+ * before 'debug' module is requiring.
+ * Because it uses it to define scope of logging
+ */
+function patchDebugScopeEnvVariable () {
+  let functionsConfig;
+  let runOnFunctionFireBaseServer = false;
+  try {
+    functionsConfig = functions.config();
+    runOnFunctionFireBaseServer = true;
+    process.env.DEBUG = _.at(
+      functionsConfig, 'debugger.scope')[0] || process.env.DEBUG;
+  } catch (e) {
+    functionsConfig = {debugger: {scope: null}};
+  }
+
+  if (runOnFunctionFireBaseServer) {
+    // we shouldn't use console
+    // but it is trade-off because we can't be sure
+    // that process.env will be patched form functions.config correctly
+    console.info(`initial process.env:
+                ${JSON.stringify(process.env)}`);
+    console.info(`initial functions.config():
+                ${JSON.stringify(functionsConfig)}`);
+  }
+}
 
 /**
  * Construct logger for a module
