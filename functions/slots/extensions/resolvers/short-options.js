7580320aedce0e62997f92d3094a7cc308ca8cc6
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-03-13 15:51:02 +0100

    add alias resolver

--- functions/slots/extensions/resolvers/short-options.js
+++ functions/slots/extensions/resolvers/short-options.js
@@ -1,5 +1,7 @@
 const humanize = require('../../../humanize');
 
+const {error} = require('../../../utils/logger')('ia:resolver:short-options');
+
 const contextProxy = require('./high-order-resolvers/context-proxy');
 
 module.exports = {
@@ -9,7 +11,12 @@ module.exports = {
    * @param context
    * @returns {Promise}
    */
-  handler: contextProxy((value) =>
-    humanize.list.toFriendlyString(value.slice(0, 3), {ends: ' or '})
-  )
+  handler: contextProxy(({value}) => {
+    if (!Array.isArray(value)) {
+      error('is not implemented yet!');
+      return undefined;
+    }
+
+    return humanize.list.toFriendlyString(value.slice(0, 3), {ends: ' or '});
+  })
 };
