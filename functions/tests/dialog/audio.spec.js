ff437915871aeb6e6299cd492a323b776a35e1c3
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-03-15 15:10:14 +0100

    reuse test options

--- functions/tests/dialog/audio.spec.js
+++ functions/tests/dialog/audio.spec.js
@@ -8,30 +8,25 @@ const mockApp = require('../_utils/mocking/app');
 
 describe('dialog', () => {
   let app;
+  let audio;
 
   beforeEach(() => {
     app = mockApp();
+    audio = {
+      audioURL: 'https://archive.org/download/gd73-06-10d1t01.mp3',
+      coverage: 'Washington, DC',
+      imageURL: 'https://archive.org/gd73-06-10.sbd.hollister.174.sbeok.shnf/RFKJune73extras/Covers/GD6-10-73backtyedie.jpg',
+      suggestions: ['rewind', 'next'],
+      title: 'Morning Dew',
+      track: 1,
+      year: 1973,
+      url: 'https://actions.google.com/sounds/v1/foley/cassette_tape_button.ogg',
+    };
   });
 
   describe('playSong', () => {
     it('should share audio response with user', () => {
-      const audioURL = 'https://archive.org/download/gd73-06-10d1t01.mp3';
-      const coverage = 'Washington, DC';
-      const imageURL = 'https://archive.org/gd73-06-10.sbd.hollister.174.sbeok.shnf/RFKJune73extras/Covers/GD6-10-73backtyedie.jpg';
-      const suggestions = ['rewind', 'next'];
-      const title = 'Morning Dew';
-      const track = 1;
-      const year = 1973;
-
-      playSong(app, {
-        audioURL,
-        coverage,
-        imageURL,
-        suggestions,
-        title,
-        track,
-        year,
-      });
+      playSong(app, audio);
 
       expect(app.ask).to.be.calledOnce;
       expect(app.buildMediaResponse).to.be.calledOnce;
@@ -40,50 +35,35 @@ describe('dialog', () => {
       expect(app.addMediaObjects).to.be.calledOnce;
       expect(app.setDescription).to.be.calledWith(
         mustache.render(mustache.render(
-          strings.description,
-          {audioURL, coverage, imageURL, suggestions, title, track, year}
+          strings.description, audio
         ))
       );
       expect(app.setImage).to.be.calledWith(
-        imageURL,
+        audio.imageURL,
         app.Media.ImageType.LARGE
       );
       expect(app.addSimpleResponse).to.be.calledWith(
         mustache.render(mustache.render(
           strings.description,
-          {audioURL, coverage, imageURL, suggestions, title, track, year}
+          audio
         ))
       );
       expect(app.addSuggestionLink).to.be.calledOnce;
-      expect(app.addSuggestions).to.be.calledWith(suggestions);
+      expect(app.addSuggestions).to.be.calledWith(audio.suggestions);
     });
 
     describe('custom speech', () => {
       it('should mute songs description speech and replace it with song', () => {
-        const audioURL = 'https://archive.org/download/gd73-06-10d1t01.mp3';
-        const coverage = 'Washington, DC';
-        const imageURL = 'https://archive.org/gd73-06-10.sbd.hollister.174.sbeok.shnf/RFKJune73extras/Covers/GD6-10-73backtyedie.jpg';
-        const suggestions = ['rewind', 'next'];
-        const title = 'Morning Dew';
-        const track = 1;
-        const year = 1973;
         const url = 'https://actions.google.com/sounds/v1/foley/cassette_tape_button.ogg';
 
-        playSong(app, {
-          audioURL,
-          coverage,
-          imageURL,
-          suggestions,
-          title,
-          track,
-          year,
+        playSong(app, Object.assign({}, audio, {
           speech: {
             mute: true,
             audio: {
               url,
             },
-          }
-        });
+          },
+        }));
 
         expect(app.addSimpleResponse).to.be.calledOnce;
         expect(app.addSimpleResponse.args[0][0])
@@ -92,9 +72,13 @@ describe('dialog', () => {
           .to.include(
             mustache.render(mustache.render(
               strings.description,
-              {audioURL, coverage, imageURL, suggestions, title, track, year}
+              audio
             ))
           );
+      });
+
+      it('should use default speech setting when mute is true', () => {
+
       });
     });
   });
