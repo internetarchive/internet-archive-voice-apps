9fc0eb8106a1e35a18a87303b0043ba9cd3184f4
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-03-15 16:36:08 +0100

    dialog response pipeline

--- functions/src/dialog/middlewares/pipeline.js
+++ functions/src/dialog/middlewares/pipeline.js
@@ -1,5 +1,32 @@
+/**
+ * Pipeline of middlewares for processing dialog response
+ */
 class Pipeline {
-  use () {
+  constructor () {
+    this.middlewares = [];
+  }
+
+  /**
+   * Process options through pipeline
+   *
+   * @param app
+   * @param options
+   * @returns {*}
+   */
+  process (app, options) {
+    return this.middlewares.reduce((acc, middleware) => {
+      return middleware(app, acc);
+    }, options);
+  }
+
+  /**
+   * Use middleware in pipeline
+   *
+   * @param middleware
+   * @returns {Pipeline}
+   */
+  use (middleware) {
+    this.middlewares.push(middleware);
     return this;
   }
 }
