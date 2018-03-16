df91a70d831ba5979fd09a3d737446a36c695651
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-03-16 16:51:20 +0100

    could choose conditional selector by option 'condition'

--- functions/tests/configurator/selectors/conditional-selector.spec.js
+++ functions/tests/configurator/selectors/conditional-selector.spec.js
@@ -1,28 +1,39 @@
 const {expect} = require('chai');
 
+const selectors = require('../../../src/configurator/selectors');
 const selector = require('../../../src/configurator/selectors/condition-selector');
 
 describe('configurator', () => {
-  describe('random selector', () => {
-    it('should choose one options for the set of available', () => {
-      const options = [
-        {
-          name: 'one',
-          condition: 'value == 1',
-        },
-        {
-          name: 'two',
-          condition: '1 < value and value < 3',
-        },
-        {
-          name: 'three',
-          condition: 'value == 3',
-        },
-      ];
-      const context = {
-        value: 2,
-      };
-      expect(selector.process(options, context)).to.be.deep.equal(options[1]);
+  describe('selectors', () => {
+    const options = [
+      {
+        name: 'one',
+        condition: 'value == 1',
+      },
+      {
+        name: 'two',
+        condition: '1 < value and value < 3',
+      },
+      {
+        name: 'three',
+        condition: 'value == 3',
+      },
+    ];
+
+    const context = {
+      value: 2,
+    };
+
+    describe('index', () => {
+      it('should choose random selector and process result on it', () => {
+        expect(selectors.process(options, context)).to.be.deep.equal(options[1]);
+      });
+    });
+
+    describe('conditional selector', () => {
+      it('should choose one options for the set of available', () => {
+        expect(selector.process(options, context)).to.be.deep.equal(options[1]);
+      });
     });
   });
 });
