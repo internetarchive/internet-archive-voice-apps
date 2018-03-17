d99c78d38303384e7dd42072ff9c99138c9cce65
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-03-17 23:40:19 +0100

    fix mute of song's title speeking

--- functions/tests/dialog/middlewares/replace-speech-if-muted.spec.js
+++ functions/tests/dialog/middlewares/replace-speech-if-muted.spec.js
@@ -2,7 +2,6 @@ const {expect} = require('chai');
 
 const middleware = require('../../../src/dialog/middlewares/replace-speech-if-muted');
 const playback = require('../../../src/state/playback');
-const strings = require('../../../src/strings').dialog.playSong;
 
 const mockApp = require('../../_utils/mocking/app');
 
@@ -11,6 +10,9 @@ describe('dialog', () => {
     describe('replace speech if muted', () => {
       let app;
       let options;
+      const strings = {
+        speech: 'Great snakes!',
+      };
 
       beforeEach(() => {
         app = mockApp();
@@ -22,13 +24,13 @@ describe('dialog', () => {
 
       it(`should left the same options if speech is not muted`, () => {
         playback.setMuteSpeechBeforePlayback(app, false);
-        expect(middleware()(app, Object.assign({}, options)))
+        expect(middleware(strings)(app, Object.assign({}, options)))
           .to.be.deep.equal(options);
       });
 
       it(`should replace speech if it's muted with template`, () => {
         playback.setMuteSpeechBeforePlayback(app, true);
-        expect(middleware()(app, Object.assign({}, options)))
+        expect(middleware(strings)(app, Object.assign({}, options)))
           .to.have.property('speech', strings.speech);
       });
     });
