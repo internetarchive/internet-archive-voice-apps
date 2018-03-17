cc4016896903caa2fe11d566486a800278e6fc61
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-03-17 23:27:07 +0100

    log middleware replace-speech-if-muted

--- functions/src/dialog/middlewares/replace-speech-if-muted.js
+++ functions/src/dialog/middlewares/replace-speech-if-muted.js
@@ -1,5 +1,6 @@
 const playback = require('../../state/playback');
 const strings = require('../../strings').dialog.playSong;
+const {debug} = require('../../utils/logger')('ia:dialog:middlewares:replace-speech-if-muted');
 
 /**
  * Process options before play audio
@@ -9,9 +10,12 @@ const strings = require('../../strings').dialog.playSong;
  * @returns {Object}
  */
 function replaceSpeechIfMutedMiddleware (app, options) {
+  debug('start');
   if (playback.isMuteSpeechBeforePlayback(app)) {
+    debug('apply');
     return Object.assign({}, options, {speech: strings.speech});
   }
+  debug('skip');
   return options;
 }
 
