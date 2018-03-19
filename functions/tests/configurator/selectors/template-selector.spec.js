d1e5d15ccdae48954c23f03836aad669f506a861
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-03-19 16:55:00 +0100

    add tests for getMatchedTemplates and getMatchedTemplatesExactly of template selector

--- functions/tests/configurator/selectors/template-selector.spec.js
+++ functions/tests/configurator/selectors/template-selector.spec.js
@@ -1,5 +1,6 @@
 const {expect} = require('chai');
 
+const extractor = require('../../../src/configurator/parsers/extract-requirements');
 const selectors = require('../../../src/configurator/selectors');
 const selector = require('../../../src/configurator/selectors/template-selector');
 
@@ -25,6 +26,86 @@ describe('configurator', () => {
         expect(selector.find(options, {
           prioritySlots: ['coverage', 'year'],
         })).to.be.equal(options[1]);
+      });
+
+      describe('getMatchedTemplates', () => {
+        it('should find list of all templates which match provides slots', () => {
+          const templates = [
+            'Album {{coverage}} {{year}}!',
+            '{{coverage}} - good place!',
+            '{{coverage}} {{year}} - great choice!',
+            '{{year}} - it was excellent year!',
+            'I love {{collection}} collection too',
+          ];
+          const slots = [
+            'coverage',
+            'year',
+          ];
+          expect(
+            selector.getMatchedTemplates(extractor.extractRequrements(templates), slots)
+          ).to.have.members([
+            'Album {{coverage}} {{year}}!',
+            '{{coverage}} - good place!',
+            '{{coverage}} {{year}} - great choice!',
+            '{{year}} - it was excellent year!',
+          ]);
+        });
+      });
+
+      describe('getMatchedTemplatesExactly', () => {
+        it('should find list of all templates which match provides slots', () => {
+          const templates = [
+            'Album {{coverage}} {{year}}!',
+            '{{coverage}} - good place!',
+            '{{coverage}} {{year}} - great choice!',
+            '{{year}} - it was excellent year!',
+            'I love {{collection}} collection too',
+          ];
+          const slots = [
+            'coverage',
+            'year',
+          ];
+          expect(
+            selector.getMatchedTemplatesExactly(extractor.extractRequrements(templates), slots)
+          ).to.have.members([
+            'Album {{coverage}} {{year}}!',
+            '{{coverage}} {{year}} - great choice!',
+          ]);
+        });
+
+        it('should match exactly templates which has all needed slots', () => {
+          const templates = [
+            'Album {{coverage}} {{year}}!',
+            '{{coverage}} - good place!',
+            '{{coverage}} {{year}} - great choice!',
+            '{{year}} - it was excellent year!',
+            'I love {{collection}} collection too',
+          ];
+          const slots = [
+            'coverage',
+          ];
+          expect(
+            selector.getMatchedTemplatesExactly(extractor.extractRequrements(templates), slots)
+          ).to.have.members([
+            '{{coverage}} - good place!',
+          ]);
+        });
+
+        it('should also respect dot notation', () => {
+          const templates = [
+            'Ok! Lets go with {{creator.title}} band!',
+          ];
+
+          const slots = [
+            'creator',
+          ];
+
+          expect(
+            selector.getMatchedTemplatesExactly(extractor.extractRequrements(templates), slots)
+          ).to.have.members([
+            'Ok! Lets go with {{creator.title}} band!',
+          ]);
+        });
       });
     });
   });
