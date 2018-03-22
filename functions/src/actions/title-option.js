257c03246f6c64979b46878459dd41df5bc3290c
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-03-22 23:27:30 +0100

    replace console.log with debug

--- functions/src/actions/title-option.js
+++ functions/src/actions/title-option.js
@@ -1,10 +1,12 @@
 const dialog = require('../dialog');
 const playback = require('../state/playback');
 const strings = require('../strings').intents.titleOption;
+const {debug} = require('../utils/logger')('ia:actions:title-option');
 
 function handler (app) {
+  debug('start');
   const value = app.getArgument('value');
-  console.log('value', value);
+  debug('got value:', value);
   playback.setMuteSpeechBeforePlayback(app, value === 'false');
   dialog.ask(app, strings[value]);
 }
