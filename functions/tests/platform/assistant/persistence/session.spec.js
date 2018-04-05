9c72e284b2e7ad2a488bfde31fab2b2d418543f0
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-04-05 23:38:53 +0200

    add test for session data persistance layer

---
+++ functions/tests/platform/assistant/persistence/session.spec.js
@@ -0,0 +1,27 @@
+const {expect} = require('chai');
+
+const mockAssistant = require('../../../_utils/mocking/app');
+const persistance = require('../../../../src/platform/assistant/persistence/session');
+
+describe('platform', () => {
+  describe('assistant', () => {
+    describe('persistance', () => {
+      describe('device level', () => {
+        it('should share state for one device', () => {
+          const device = mockAssistant({deviceId: 'device'});
+          persistance(device).setData('value', 'hello world');
+          expect(persistance(device).getData('value')).to.be.equal('hello world');
+        });
+
+        it('should share state for one device', () => {
+          const device1 = mockAssistant({deviceId: 'device1'});
+          const device2 = mockAssistant({deviceId: 'device2'});
+          persistance(device1).setData('value', '1');
+          persistance(device2).setData('value', '2');
+          expect(persistance(device1).getData('value'))
+            .to.not.be.equal(persistance(device2).getData('value'));
+        });
+      });
+    });
+  });
+});
