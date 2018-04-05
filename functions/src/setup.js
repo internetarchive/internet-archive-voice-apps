2ae868ecc85d134c77d00f102fc4bc9ce4f4d99c
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-04-05 20:11:12 +0200

    send user agent in a format {name} / {version} {platform}

--- functions/src/setup.js
+++ functions/src/setup.js
@@ -2,17 +2,26 @@
  * Setup application
  */
 const axios = require('axios');
+const mustache = require('mustache');
 
-const env = require('./config/env');
+const packageJSON = require('../package.json');
+
+const appConfig = require('./config');
 const mathjsExtensions = require('./mathjs');
+const whichPlatform = require('./platform/which');
 const {debug, warning} = require('./utils/logger')('ia:axio:interceptions');
 
 module.exports = () => {
   mathjsExtensions.patch();
 
+  const userAgent = mustache.render(
+    appConfig.request.userAgent,
+    Object.assign({}, packageJSON, {platform: whichPlatform()})
+  );
+
   // patch requests
   axios.interceptors.request.use((config) => {
-    config.headers['user-agent'] = env('request', 'user_agent');
+    config.headers['user-agent'] = userAgent;
     debug(`${config.method.toUpperCase()} ${config.url}`);
     return config;
   }, (error) => {
