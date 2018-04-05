0d77113c4613986fd26bcd1ee958006a649c8bda
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-04-05 19:45:19 +0200

    debug request and fails

--- functions/src/setup.js
+++ functions/src/setup.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 
 const env = require('./config/env');
 const mathjsExtensions = require('./mathjs');
+const {debug, warning} = require('./utils/logger')('ia:axio:interceptions');
 
 module.exports = () => {
   mathjsExtensions.patch();
@@ -12,8 +13,10 @@ module.exports = () => {
   // patch requests
   axios.interceptors.request.use((config) => {
     config.headers['user-agent'] = env('request', 'user_agent');
+    debug(`${config.method.toUpperCase()} ${config.url}`);
     return config;
   }, (error) => {
+    warning(`fail request ${error.config.method.toUpperCase()} ${error.config.url}`, error);
     return Promise.reject(error);
   });
 };
