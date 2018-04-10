fdcf28852bc689e76c58f5ae2c5ed61c0f574a66
Bimlendra <bimlendra.singh@gmail.com>
2018-04-10 21:54:20 +0530

    made changes as per new flow

--- functions/src/utils/logger/log-app-start.js
+++ functions/src/utils/logger/log-app-start.js
@@ -1,4 +1,5 @@
 const {debug} = require('./')('ia:start');
+const packageJSON = require('../../../package.json');
 
 module.exports = (actionsMap) => {
   const actionNames = Array.from(actionsMap.keys())
@@ -9,6 +10,7 @@ module.exports = (actionsMap) => {
   debug('-----------------------------------------');
   debug(`Node.js Version: ${process.version}`);
   debug('-----------------------------------------');
-
+  debug(`DialogFlow App Version: ${packageJSON.version}`);
+  debug('-----------------------------------------');
   debug(`We can handle actions: ${actionNames}`);
 };
--- functions/tests/actions/version.spec.js
+++ functions/tests/actions/version.spec.js
@@ -3,7 +3,7 @@ const rewire = require('rewire');
 
 const version = rewire('../../src/actions/version');
 
-const mockApp = require('../_utils/mocking/app');
+const mockApp = require('../_utils/mocking/platforms/assistant');
 const mockDialog = require('../_utils/mocking/dialog');
 
 describe('actions', () => {
