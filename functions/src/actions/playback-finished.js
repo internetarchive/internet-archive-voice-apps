dbc27ebd39704db3ef29afeb3ba3c76a6cd4c815
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-04-12 14:24:59 +0200

    add handle of playback finished

---
+++ functions/src/actions/playback-finished.js
@@ -0,0 +1,16 @@
+const {debug} = require('../utils/logger')('ia:actions:playback-finished');
+
+function handler (app) {
+  // TODO: set playbackFinished
+  // could be useful to resume playback
+  // https://github.com/alexa/skill-sample-nodejs-audio-player/blob/1da04690933aab0a2711be6075becf67004a1896/multiple-streams/lambda/src/stateHandlers.js#L83
+  debug('token', app.params.getByName('token'));
+}
+
+/**
+ * handle Alexa AudioPlayer.PlaybackFinished intent
+ * @type {{handler: handler}}
+ */
+module.exports = {
+  handler,
+};
