9b6ff825fd1214e38569ebdffd0241cb19aa5697
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-04-21 19:50:59 +0200

    migrate handler

--- functions/src/platform/alexa/handler/index.js
+++ functions/src/platform/alexa/handler/index.js
@@ -7,37 +7,41 @@ const handlersBuilder = require('./handlers-builder');
 
 module.exports = (actions) => {
   const handlers = handlersBuilder(actions);
+  let skill;
   debug(`We can handle intents: ${Object.keys(handlers).map(name => `"${name}"`).join(', ')}`);
-  return (event, context, callback) => {
-    const alexa = Alexa.handler(event, context, callback);
 
+  return (event, context) => {
     info('request type:', event.request.type);
     if (event.request.intent) {
       info('request intent:', event.request.intent.name);
+    }
 
-      // if intent starts with AMAZON we will cut this head
-      const parts = event.request.intent.name.split('.');
-      if (parts[0] === 'AMAZON') {
-        debug('cut AMAZON head from intent');
-        event.request.intent.name = parts.slice(1).join('.');
+    if (!skill) {
+      debug('lazy building');
+
+      skill = Alexa.SkillBuilders.custom()
+        .addRequestHandlers(...handlers)
+        // .addErrorHandlers(ErrorHandler)
+        // TODO: get from process.env
+        // .withSkillId()
+        .create();
+
+      const region = process.env.AWS_REGION;
+      if (region) {
+        debug('set AWS region', region);
+        AWS.config.update({region});
       }
-    }
 
-    // TODO: get from process.env
-    // alexa.appId
-    const region = process.env.AWS_REGION;
-    if (region) {
-      debug('set AWS region', region);
-      AWS.config.update({region});
+      // TODO: we don't need it for session attributes
+      // turn on once we will have persistant attributes
+      // alexa.dynamoDBTableName = 'InternetArchiveSessions';
     }
-    alexa.dynamoDBTableName = 'InternetArchiveSessions';
-    alexa.registerHandlers(handlers);
 
     try {
-      alexa.execute();
+      return skill.invoke(event, context);
     } catch (err) {
       error('Caught Error:', err);
-      alexa.emit(':tell', 'Sorry, I\'m experiencing some technical difficulties at the moment. Please try again later.');
+      // alexa.emit(':tell', 'Sorry, I\'m experiencing some technical difficulties at the moment. Please try again later.');
     }
   };
 };
