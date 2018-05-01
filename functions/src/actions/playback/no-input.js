6ee63f903f27fef81d568702b4960c2eb6deafd3
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-05-01 22:47:44 +0200

    should resume playback on no-input in context of playback fsm state

---
+++ functions/src/actions/playback/no-input.js
@@ -0,0 +1,30 @@
+const dialog = require('../../dialog');
+const dialogState = require('../../state/dialog');
+const strings = require('../../strings');
+const {debug} = require('../../utils/logger')('ia:actions:playback/no-input');
+
+const feederFromPlaylist = require('../high-order-handlers/middlewares/feeder-from-playlist');
+
+const helpers = require('./_helpers');
+
+function handler (app) {
+  return helpers.playSong({app, next: false})
+    .catch(err => {
+      if (err instanceof feederFromPlaylist.EmptyFeederError) {
+        dialog.ask(app, dialog.merge(
+          strings.intents.resume.empty,
+          dialogState.getReprompt(app)
+        ));
+      } else {
+        debug('It could be an error:', err);
+        return dialog.ask(app, dialog.merge(
+          strings.intents.resume.fail,
+          dialogState.getReprompt(app)
+        ));
+      }
+    });
+}
+
+module.exports = {
+  handler,
+};
