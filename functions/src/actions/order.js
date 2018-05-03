ae257f217b1245807a446fdba84dead6b55fd97f
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-05-03 18:48:28 +0200

    by default reprompt on order intent

--- functions/src/actions/order.js
+++ functions/src/actions/order.js
@@ -1,7 +1,11 @@
+const dialog = require('../dialog');
+const {getLastPhrase} = require('../state/dialog');
+const strings = require('../strings').intents.order;
 const {debug} = require('../utils/logger')('ia:actions:order');
 
-function handler(app) {
+function handler (app) {
   debug('order in context of default state!');
+  dialog.ask(app, dialog.merge(strings, getLastPhrase(app)));
 }
 
 module.exports = {
--- functions/src/actions/search-music/order.js
+++ functions/src/actions/search-music/order.js
@@ -1,6 +1,6 @@
 const {debug} = require('../../utils/logger')('ia:actions:search-music/order');
 
-function handler(app) {
+function handler (app) {
   debug('order in context of search music state!');
 }
 
