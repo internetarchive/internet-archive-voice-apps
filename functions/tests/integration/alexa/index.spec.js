78c24671a737e6d6a647fe1427d16460c1dbe3df
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-05-08 19:46:18 +0200

    use yaml for test dialogs

--- functions/tests/integration/alexa/index.spec.js
+++ functions/tests/integration/alexa/index.spec.js
@@ -1,12 +1,22 @@
+const dynamoDbPersistenceAdapter = require('ask-sdk-dynamodb-persistence-adapter');
 const axios = require('axios');
 const MockAdapter = require('axios-mock-adapter');
-const dynamoDbPersistenceAdapter = require('ask-sdk-dynamodb-persistence-adapter');
+const fs = require('fs');
 const expect = require('chai').expect;
+const yaml = require('js-yaml');
+const path = require('path');
 const sinon = require('sinon');
 const VirtualAlexa = require('virtual-alexa').VirtualAlexa;
 
 const popularAlbums = require('../../provider/fixtures/popular-of-etree.json');
 const DynamoDBMock = require('../../_utils/mocking/dynamodb');
+
+let tests = [];
+try {
+  tests = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, 'dialog.yaml'), 'utf8'));
+} catch (e) {
+  console.log(e);
+}
 
 describe('integration', () => {
   let alexa;
@@ -48,13 +58,19 @@ describe('integration', () => {
         })
     );
 
-    it('Should utter "live collection" and get a response', () => {
-      return alexa
-        .utter('live collection')
-        .then(res => {
-          expect(res.response.outputSpeech.ssml).to.exist;
-          expect(res.response.outputSpeech.ssml).to.include('You\'ve selected Live Concerts. What artist would you like to listen to? For example, the Grateful Dead, the Phil Lesh and Friends or the Disco Biscuits?');
-        })
+    tests.forEach(({scenario, dialog}) => {
+      describe(`scanario "${scenario}"`, () => {
+        dialog.forEach(({user, assistant}) => {
+          it(`should utter: "${user}" and get a response: "${assistant}"`, () => {
+            return alexa
+              .utter('live collection')
+              .then(res => {
+                expect(res.response.outputSpeech.ssml).to.exist;
+                expect(res.response.outputSpeech.ssml).to.include(assistant);
+              });
+          });
+        });
+      });
     });
   });
 });
