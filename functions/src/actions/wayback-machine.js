c2b2cae413806985031bf4bf22abc47c2d3aa380
adaveinthelife <dave.barry@uga.edu>
2018-06-15 20:00:24 -0400

    Rewrote wayback machine to return promise

--- functions/src/actions/wayback-machine.js
+++ functions/src/actions/wayback-machine.js
@@ -1,10 +1,121 @@
 const request = require('request');
 const traverse = require('traverse');
 const xml2js = require('xml2js');
-// const dialog = require('../dialog');
+const dialog = require('../dialog');
 const mustache = require('mustache');
 const waybackStrings = require('../strings').intents.wayback;
 const {debug} = require('../utils/logger')('ia:actions:wayback-machine');
+
+/**
+ * Handle wayback query action
+ * - fill slots of wayback query
+ * - perform data requests to archive and alexa rankings
+ * - construct response speech for action
+ *
+ * @param app
+ */
+function handler (app) {
+
+  // Check parameter for Wayback qualifier
+  let wayback = app.params.getByName('wayback');
+  if (wayback.includes('wayback') === false) {
+    debug('wayback action called by mistake');
+  }
+
+  //Get url parameter
+  var url = app.params.getByName('url');
+
+  var waybackObject = {
+    url: '',
+    earliestYear: 0,
+    latestYear: 0,
+    totalUniqueURLs: 0,
+    alexaWorldRank: 0,
+    alexaUSRank: 0,
+    speech: 'default speech'
+  };
+
+  waybackObject.url = url;
+  var archiveQueryURL = 'http://web.archive.org/__wb/search/metadata?q=' + url;
+  var alexaQueryURL = 'http://data.alexa.com/data?cli=10&url=' + url;
+
+
+return Promise.all([getRequest(archiveQueryURL), getRequest(alexaQueryURL)])
+    .then(function(allData) {
+        // All data available here in the order it was called.
+
+        // Parse data from archive request
+        var archiveJSON = JSON.parse(allData[0]);
+        archiveEngine(archiveJSON, waybackObject);
+
+        // Parse data from alexa request
+        var alexaJSON;
+        var XMLparser = new xml2js.Parser();
+        XMLparser.parseString(allData[1], function (err, result) {
+          if (err) {
+            debug('Uh-oh, the XML parser didn\'t work');
+          } else {
+            alexaJSON = JSON.parse(JSON.stringify(result));
+          }
+        });
+        alexaEngine(alexaJSON, waybackObject);
+
+        /*
+        debug('ARCHIVE');
+        debug('Date of first archive: ' + waybackObject.earliestYear);
+        debug('Date of last archive: ' + waybackObject.latestYear);
+        debug('Total Unique URLs: ' + waybackObject.totalUniqueURLs);
+        debug('ALEXA');
+        debug('Alexa World Rank: ' + waybackObject.alexaWorldRank);
+        debug('Alexa US Rank: ' + waybackObject.alexaUSRank);
+        */
+
+        // Construct response dialog for action
+        waybackObject.speech = mustache.render(waybackStrings.speech, waybackObject);
+        //debug('speech:' + waybackObject.speech);
+
+        dialog.close(app, waybackObject);
+    });
+} //End of handler
+
+function getRequest (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
+function archiveEngine(archiveJSON, waybackObject) {
+  // Create array of capture years and then find earliest year
+  //  and most recent year.
+  var yearsArray = Object.keys(archiveJSON.captures);
+  waybackObject.earliestYear = yearsArray[0];
+  waybackObject.latestYear = yearsArray[yearsArray.length - 1];
+
+  // Traverse URL category to find baseline of URL count
+  traverse(archiveJSON.urls[waybackObject.earliestYear]).forEach(function (node) {
+    if (typeof node === 'number') {
+      waybackObject.totalUniqueURLs += node;
+    }
+  });
+  // debug('Beginning value for total unique urls: ' + totalUniqueURLs);
+
+  traverse(archiveJSON.new_urls).forEach(function (node) {
+    if (typeof node === 'number') {
+      waybackObject.totalUniqueURLs += node;
+    }
+  });
+}
+
+function alexaEngine(alexaJSON, waybackObject) {
+  waybackObject.alexaWorldRank = alexaJSON['ALEXA']['SD'][0]['POPULARITY'][0]['$']['TEXT'];
+  waybackObject.alexaUSRank = alexaJSON['ALEXA']['SD'][0]['COUNTRY'][0]['$']['RANK'];
+}
 
 /**
  * Handle wayback query action
@@ -14,6 +125,7 @@ const {debug} = require('../utils/logger')('ia:actions:wayback-machine');
  *
  * @param app
  */
+ /*
 function handler (app) {
   // Check parameter for Wayback qualifier
   let wayback = app.params.getByName('wayback');
@@ -80,13 +192,14 @@ function handler (app) {
         waybackObject.alexaUSRank = convertedJSON['ALEXA']['SD'][0]['COUNTRY'][0]['$']['RANK'];
 
         waybackObject.speech = mustache.render(waybackStrings.speech, waybackObject);
-        // debug('speech:' + waybackObject.speech);
+        debug('speech:' + waybackObject.speech);
         // dialog.close(app, waybackObject);
       }
     });
   }); // End of request
 
-  // dialog.close(app, )
+  // Give wayback object to dialog function
+  //dialog.close(app, waybackObject);
 } // End of handler
 function getRequest (url) {
   return new Promise(function (resolve, reject) {
@@ -99,6 +212,7 @@ function getRequest (url) {
     });
   });
 }
+*/
 
 module.exports = {
   handler
