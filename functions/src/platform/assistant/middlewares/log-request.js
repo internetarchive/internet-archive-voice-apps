94f918a8954644b65867269f8075d13663a3ff5b
adaveinthelife <dave.barry@uga.edu>
2018-07-31 14:08:15 -0400

    Modified log-request to use conv.user.storage instead of conv.user.id which is depreciated

--- functions/src/platform/assistant/middlewares/log-request.js
+++ functions/src/platform/assistant/middlewares/log-request.js
@@ -14,7 +14,7 @@ module.exports = (conv) => {
   debug('\n\n');
   info(`start handling action: ${conv.action}, intent: ${conv.intent}`);
   if (conv.user) {
-    info(`user.id:`, conv.user.id);
+    info(`user.id:`, conv.user.storage);
     info(`user.last:`, conv.user.last);
     info(`user.name:`, util.inspect(conv.user.name));
   } else {
