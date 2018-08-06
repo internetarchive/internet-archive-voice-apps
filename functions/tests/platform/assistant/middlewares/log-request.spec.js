5aa9a1a9c0617dedc94c0c52937bef313a2afe29
adaveinthelife <dave.barry@uga.edu>
2018-08-06 10:39:47 -0400

    Made first iteration of spec test file for new conv.id check in loq-request.js

---
+++ functions/tests/platform/assistant/middlewares/log-request.spec.js
@@ -0,0 +1,23 @@
+const {expect} = require('chai');
+const rewire = require('rewire');
+
+const mockApp = require('../../../_utils/mocking/platforms/app');
+const mockDialog = require('../_utils/mocking/dialog');
+
+const logRequest = rewire('../../../../src/platform/assistant/middlewares/log-request.js');
+
+describe('platform', () => {
+  describe('assistant', () => {
+    let dialog;
+
+    beforeEach(() => {
+      dialog = mockDialog();
+      logRequest.__set__('dialog', dialog);
+    });
+
+    it('should check if a user id is present', () => {
+      let app = mockApp();
+      logRequest.handler(app);
+    });
+  });
+});
