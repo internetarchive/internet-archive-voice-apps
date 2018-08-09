007ad3f7878e3b0fb1fda11cc793e09d12fb623d
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-08-09 14:05:14 +0200

    http error should inherit error class

--- functions/src/errors.js
+++ functions/src/errors.js
@@ -1,5 +1,11 @@
-class HTTPError {
+class HTTPError extends Error {
   constructor (original) {
+    super();
+    // Maintains proper stack trace for where our error was thrown (only available on V8)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, HTTPError);
+    }
+
     this.original = original;
   }
 }
