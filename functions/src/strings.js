e6baa2c5d09a2a9c2518dd331319adf7e6a83782
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-11-06 21:33:13 +0100

    find music records phrase

--- functions/src/strings.js
+++ functions/src/strings.js
@@ -93,7 +93,7 @@ module.exports = {
       // but on Google Assistant it would be The Internet Archive Action
       speech:
         '<s>{{platform.appName}} can not help with that.</s> ' +
-        '<s>But I can help you to find music record <break strength="weak"/> and play it.</s> ' +
+        '<s>But I can help you to find music records <break strength="weak"/> and play it.</s> ' +
         '<s>{{last.reprompt}}</s>'
     },
 
