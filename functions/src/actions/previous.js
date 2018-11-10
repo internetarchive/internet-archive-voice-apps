39690f7e59f3d0a29513be474925851d363ecda7
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-11-10 09:15:17 +0100

    handle previous song intent

---
+++ functions/src/actions/previous.js
@@ -0,0 +1,22 @@
+const dialog = require('../dialog');
+const strings = require('../strings');
+const { debug } = require('../utils/logger')('ia:actions:next');
+
+const helpers = require('./playback/_helpers');
+
+/**
+ * handle playback failed request
+ *
+ * @param app
+ */
+function handler (app) {
+  return helpers.playSong({ app, next: true })
+    .catch(e => {
+      debug('It could be an error:', e);
+      return dialog.ask(app, strings.events.playlistIsEnded);
+    });
+}
+
+module.exports = {
+  handler,
+};
