53121bb3210bc76062031635243d57d60ff55498
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-11-15 15:21:40 +0100

    turn on loop on loop on intent

--- functions/src/actions/loop-on.js
+++ functions/src/actions/loop-on.js
@@ -1,7 +1,10 @@
 const strings = require('../strings');
+const playlist = require('../state/playlist');
+
 const helpers = require('./_helpers');
 
 function handler (app) {
+  playlist.setLoop(app, true);
   helpers.simpleResponse(app, strings.intents.loopOn.default);
 }
 
