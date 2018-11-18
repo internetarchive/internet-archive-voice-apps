f90d24e13db5da6dbfdb47fe2717307932b0397a
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-11-18 13:02:35 +0100

    fix sync prefetching of album

--- functions/src/extensions/feeders/_default.js
+++ functions/src/extensions/feeders/_default.js
@@ -70,12 +70,17 @@ class DefaultFeeder {
    *
    * TODO: should be async because we could have multiple albums here
    *
+   * @param ctx
+   * @param move
    * @returns {Promise.<T>}
    */
-  next ({ app, slots, playlist }, move = true) {
-    debug('move to the next song');
-    playlist.next(app);
-    return Promise.resolve();
+  next (ctx, move = true) {
+    const { app, playlist } = ctx;
+    if (move) {
+      debug('move to the next song');
+      playlist.next(app);
+    }
+    return Promise.resolve(ctx);
   }
 
   /**
@@ -85,10 +90,11 @@ class DefaultFeeder {
    *
    * @returns {Promise.<T>}
    */
-  previous ({ app, slots, playlist }) {
+  previous (ctx) {
+    const { app, playlist } = ctx;
     debug('move to the previous song');
     playlist.previous(app);
-    return Promise.resolve();
+    return Promise.resolve(ctx);
   }
 
   /**
