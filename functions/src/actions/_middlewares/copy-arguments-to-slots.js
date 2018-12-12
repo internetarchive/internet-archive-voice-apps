654a363eebef16283576cb2efe13f6262f1f273b
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-12-13 00:02:08 +0100

    refactor caopy args to slots middleware

--- functions/src/actions/_middlewares/copy-arguments-to-slots.js
+++ functions/src/actions/_middlewares/copy-arguments-to-slots.js
@@ -12,27 +12,20 @@ const { debug } = require('../../utils/logger/index')('ia:actions:middleware:cop
  * @param app.slotScheme
  * @returns {Promise.<{app: *, newValues, query: *, slotScheme: *}>}
  */
-module.exports = () =>
-// sadly Google Firebase doesn't support modern Node.js
-//   ({app, newValues = {}, query, slotScheme, ...res}) => {
-  args => {
-    debug('start');
-    let { app, newValues = {}, query, slotScheme } = args;
-    debug(`we have [${slotScheme.slots}] to check`);
-    newValues = slotScheme.slots
-      .reduce((newValues, slotName) => {
-        let value = app.params.getByName(slotName);
-        if (value) {
-          query.setSlot(app, slotName, value);
-          newValues = Object.assign({}, newValues, { [slotName]: value });
-        }
-        return newValues;
-      }, newValues);
+module.exports = () => args => {
+  debug('start');
+  let { app, newValues = {}, query, slotScheme } = args;
+  debug(`we have [${slotScheme.slots}] to check`);
+  newValues = slotScheme.slots
+    .reduce((newValues, slotName) => {
+      let value = app.params.getByName(slotName);
+      if (value) {
+        query.setSlot(app, slotName, value);
+        newValues[slotName] = value;
+      }
+      return newValues;
+    }, { ...newValues });
 
-    debug(`and copied ${util.inspect(newValues)} slot(s)`);
-    return Promise.resolve(
-      Object.assign({}, args, { newValues })
-    );
-    // sadly Google Firebase doesn't support modern Node.js
-    // return Promise.resolve({app, newValues, query, slotScheme, ...res});
-  };
+  debug(`and copied ${util.inspect(newValues)} slot(s)`);
+  return Promise.resolve({ ...args, newValues })
+};
