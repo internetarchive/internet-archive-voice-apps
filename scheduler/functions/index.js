26f5621997de92b3a6d8f90604947a1fc34a93f7
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2019-09-09 15:15:54 +0200

    fix(cleaner): use _ instead of lodash

--- scheduler/functions/index.js
+++ scheduler/functions/index.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
-const lodash = require('lodash');
+const _ = require('lodash');
 const moment = require('moment');
 
 
