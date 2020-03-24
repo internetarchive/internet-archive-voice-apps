050e2721e4a5ee21cef3ac8de724ba163ae772be
UmerMIB <mohammadumer704@gmail.com>
2020-03-24 01:09:57 -0700

    fixed trailing issue

--- functions/src/strings.js
+++ functions/src/strings.js
@@ -112,7 +112,7 @@ module.exports = {
       // because in alexa it is The Internet Archive Skill
       // but on Google Assistant it would be The Internet Archive Action
       speech:
-        '<s>Oops! You are still in Internet Archive Service. Do you want to continue?</s>' + 
+        '<s>Oops! You are still in Internet Archive Service. Do you want to continue?</s>' +
         '<s>{{last.reprompt}}</s>',
     },
 
