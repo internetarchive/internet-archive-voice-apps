1abc982394eab8a730d5a70ebe4530b1b1499ee9
Vangelis Banos <vangelis@archive.org>
2023-05-15 06:32:32 +0000

    Drop node-fetch and moment dependencies
    node-fetch wasn't used at all.
    
    moment was used in a minor place. It could be dropped.

--- functions/src/platform/assistant/middlewares/log-session-duration.js
+++ functions/src/platform/assistant/middlewares/log-session-duration.js
@@ -1,9 +1,6 @@
-const moment = require('moment');
-
 const { debug } = require('../../../utils/logger')('ia:platform.assistant.middlewares.log-session-duration');
 
 module.exports = (conv) => {
   const delta = Date.now() - conv.firestore.sessionData.createdAt;
-  const timePass = moment(new Date(conv.firestore.sessionData.createdAt)).fromNow(true);
-  debug(`session runs for ${timePass} / ${delta} ms`);
+  debug(`session runs for ${delta} ms`);
 };
